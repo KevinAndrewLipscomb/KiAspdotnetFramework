fee14f92d404ffe2f7fc9c505dfee1caa35f6d24
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2008-06-16 20:05:48 +0000

    Included the js folder into the deployment list.

--- trunk/framework/aspdotnet/ki/deploy.bat
+++ trunk/framework/aspdotnet/ki/deploy.bat
@@ -9,6 +9,7 @@
 @robocopy C:\Inetpub\wwwroot\KiAspdotnetFramework                                  ready-to-deploy                                 *.pdf                     /m /np /ns /z | qgrep "New "
 @robocopy C:\Inetpub\wwwroot\KiAspdotnetFramework\bin                              ready-to-deploy\bin                                                       /m /np /ns /z | qgrep "New "
 @robocopy C:\Inetpub\wwwroot\KiAspdotnetFramework\image                            ready-to-deploy\image                                                     /m /np /ns /z | qgrep "New "
+@robocopy C:\Inetpub\wwwroot\KiAspdotnetFramework\js                               ready-to-deploy\js                                                        /m /np /ns /z | qgrep "New "
 @robocopy C:\Inetpub\wwwroot\KiAspdotnetFramework\noninteractive                   ready-to-deploy\noninteractive                  *.crontab                 /m /np /ns /z | qgrep "New "
 @robocopy C:\Inetpub\wwwroot\KiAspdotnetFramework\noninteractive                   ready-to-deploy\noninteractive                  *.aspx                    /m /np /ns /z | qgrep "New "
 @robocopy C:\Inetpub\wwwroot\KiAspdotnetFramework\protected                        ready-to-deploy\protected                       *.asax                    /m /np /ns /z | qgrep "New "
@@ -17,7 +18,7 @@
 @robocopy C:\Inetpub\wwwroot\KiAspdotnetFramework\protected\image                  ready-to-deploy\protected\image                                           /m /np /ns /z | qgrep "New "
 @robocopy C:\Inetpub\wwwroot\KiAspdotnetFramework\protected\template\notification  ready-to-deploy\protected\template\notification                           /m /np /ns /z | qgrep "New "
 @robocopy C:\Inetpub\wwwroot\KiAspdotnetFramework\resource                         ready-to-deploy\resource                                                  /m /np /ns /z | qgrep "New "
-@robocopy C:\Inetpub\wwwroot\KiAspdotnetFramework\template\notification                     ready-to-deploy\template\notification                                     /m /np /ns /z | qgrep "New "
+@robocopy C:\Inetpub\wwwroot\KiAspdotnetFramework\template\notification            ready-to-deploy\template\notification                                     /m /np /ns /z | qgrep "New "
 @robocopy C:\Inetpub\wwwroot\KiAspdotnetFramework\usercontrol\app                  ready-to-deploy\usercontrol\app                 *.ascx                    /m /np /ns /z | qgrep "New "
 @robocopy C:\Inetpub\wwwroot\KiAspdotnetFramework\usercontrol\ki                   ready-to-deploy\usercontrol\ki                  *.ascx                    /A /np /ns /z | qgrep "New "
 @pause
