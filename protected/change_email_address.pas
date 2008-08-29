94f1286c88aa450cc51eb744eff76c2ddfb87f71
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2008-08-29 20:01:58 +0000

    Improved BindDirectToListControl in template_trivial_items.  Removed obsolete objects.

--- protected/change_email_address.pas
+++ protected/change_email_address.pas
@@ -22,8 +22,6 @@ type
     procedure InitializeComponent;
     procedure Button_submit_Click(sender: System.Object; e: System.EventArgs);
     procedure CustomValidator_nominal_email_address_ServerValidate(source: System.Object; args: System.Web.UI.WebControls.ServerValidateEventArgs);
-    procedure LinkButton_back_to_overview_Click(sender: System.Object; e: System.EventArgs);
-    procedure LinkButton_logout_Click(sender: System.Object; e: System.EventArgs);
     procedure TWebForm_change_email_address_PreRender(sender: System.Object;
       e: System.EventArgs);
   {$ENDREGION}
@@ -32,17 +30,14 @@ type
     procedure Page_Load(sender: System.Object; e: System.EventArgs);
   strict protected
     Title: System.Web.UI.HtmlControls.HtmlTitle;
-    Label_account_descriptor: System.Web.UI.WebControls.Label;
     Button_submit: System.Web.UI.WebControls.Button;
     TextBox_nominal_email_address: System.Web.UI.WebControls.TextBox;
     TextBox_confirmation_email_address: System.Web.UI.WebControls.TextBox;
     RequiredFieldValidator_nominal_email_address: System.Web.UI.WebControls.RequiredFieldValidator;
     RequiredFieldValidator_confirmation_email_address: System.Web.UI.WebControls.RequiredFieldValidator;
     RegularExpressionValidator_nominal_email_address: System.Web.UI.WebControls.RegularExpressionValidator;
-    LinkButton_back_to_overview: System.Web.UI.WebControls.LinkButton;
     CustomValidator_nominal_email_address: System.Web.UI.WebControls.CustomValidator;
-    CompareValidator1: System.Web.UI.WebControls.CompareValidator;
-    LinkButton_logout: System.Web.UI.WebControls.LinkButton;
+    CompareValidator_confirmation_email_address: System.Web.UI.WebControls.CompareValidator;
   protected
     procedure OnInit(e: EventArgs); override;
   private
@@ -59,12 +54,10 @@ implementation
 /// </summary>
 procedure TWebForm_change_email_address.InitializeComponent;
 begin
-  Include(Self.LinkButton_logout.Click, Self.LinkButton_logout_Click);
-  Include(Self.LinkButton_back_to_overview.Click, Self.LinkButton_back_to_overview_Click);
   Include(Self.CustomValidator_nominal_email_address.ServerValidate, Self.CustomValidator_nominal_email_address_ServerValidate);
   Include(Self.Button_submit.Click, Self.Button_submit_Click);
-  Include(Self.Load, Self.Page_Load);
   Include(Self.PreRender, Self.TWebForm_change_email_address_PreRender);
+  Include(Self.Load, Self.Page_Load);
 end;
 {$ENDREGION}
 
@@ -75,13 +68,9 @@ begin
   case NatureOfVisit('change_email_password.p') of
   VISIT_INITIAL:
     BEGIN
-    Title.InnerText := configurationmanager.appsettings['application_name'] + ' - change_email_address';
+    Title.text := configurationmanager.appsettings['application_name'] + ' - change_email_address';
     p.biz_user:= TClass_biz_user.Create;
     p.biz_users := TClass_biz_users.Create;
-    //
-    // Set Label_account descriptor
-    //
-    Label_account_descriptor.Text := session['username'].ToString;
     //
     // Preload email address fields
     //
@@ -111,20 +100,6 @@ procedure TWebForm_change_email_address.TWebForm_change_email_address_PreRender(
   e: System.EventArgs);
 begin
   SessionSet('change_email_password.p',p);
-end;
-
-procedure TWebForm_change_email_address.LinkButton_logout_Click(sender: System.Object;
-  e: System.EventArgs);
-begin
-  formsauthentication.SignOut;
-  session.Clear;
-  server.Transfer('../Default.aspx');
-end;
-
-procedure TWebForm_change_email_address.LinkButton_back_to_overview_Click(sender: System.Object;
-  e: System.EventArgs);
-begin
-  server.Transfer('overview.aspx');
 end;
 
 procedure TWebForm_change_email_address.CustomValidator_nominal_email_address_ServerValidate(source: System.Object;
