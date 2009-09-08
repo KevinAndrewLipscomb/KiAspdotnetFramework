455d00ebf70c15417aa23e83079af5530a6b03b3
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2009-09-08 14:40:33 +0000

    Switched to C# codebehind.

---
+++ component/biz/Class_biz_scheduled_tasks.cs
@@ -0,0 +1,20 @@
+using System;
+namespace Class_biz_scheduled_tasks
+{
+    public class TClass_biz_scheduled_tasks
+    {
+        //Constructor  Create()
+        public TClass_biz_scheduled_tasks() : base()
+        {
+            // TODO: Add any constructor code here
+
+        }
+        public void DoDailyChores()
+        {
+            // Call db class methods to perform chores.
+
+        }
+
+    } // end TClass_biz_scheduled_tasks
+
+}
