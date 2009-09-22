b543328bce4735a96f46d21a64b48f3ca7eb23df
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2009-09-22 19:52:54 +0000

    Revised to open SLN file instead of DPROJ file.

--- trunk/framework/aspdotnet/ki/work.bat
+++ trunk/framework/aspdotnet/ki/work.bat
@@ -4,5 +4,5 @@ REM
 cd "C:\Inetpub\wwwroot\KiAspdotnetFramework"
 start /max explorer /e,/select,C:\Inetpub\wwwroot\KiAspdotnetFramework\.svn
 start /max explorer ftp://ftp.frompaper2web.com/ssl/KiAspdotnetFramework_x
-start /max KiAspdotnetFramework.dproj
+start /max KiAspdotnetFramework.sln
 start "" /max "C:\Program Files\MySQL\MySQL Tools for 5.0\MySQLQueryBrowser.exe"
