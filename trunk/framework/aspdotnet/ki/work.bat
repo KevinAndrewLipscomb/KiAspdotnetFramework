4aa64f5d171fd943141afcb3afc93d493eebdb11
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2009-10-04 18:53:37 +0000

    Revised to use Filezilla since IE8 FTP has credential-saving issues.

--- trunk/framework/aspdotnet/ki/work.bat
+++ trunk/framework/aspdotnet/ki/work.bat
@@ -3,6 +3,6 @@ REM $Id$
 REM
 cd "C:\Inetpub\wwwroot\KiAspdotnetFramework"
 start /max explorer /e,/select,C:\Inetpub\wwwroot\KiAspdotnetFramework\.svn
-start /max explorer ftp://ftp.frompaper2web.com/ssl/KiAspdotnetFramework_x
+start "" /max "C:\Program Files\FileZilla FTP Client\filezilla.exe" --site=0/ftp.frompaper2web.com\/ssl/KiAspdotnetFramework_x
 start /max KiAspdotnetFramework.sln
 start "" /max "C:\Program Files\MySQL\MySQL Tools for 5.0\MySQLQueryBrowser.exe"
