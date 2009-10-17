8816e06654c11ea255a8164e411466fa1fbbcea6
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2009-10-17 19:19:33 +0000

    Updated to default to Autoeventwireup=True, and added a row counter literal.  Created CS templates.

---
+++ trunk/framework/aspdotnet/ki/usercontrol/app/UserControl_template_datagrid_sortable.ascx.cs
@@ -0,0 +1,227 @@
+// Derived from KiAspdotnetFramework/UserControl/app/UserControl~template~datagrid~sortable.ascx.cs
+
+using Class_biz_0s;
+using kix;
+using System;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Collections;
+
+namespace UserControl_template_datagrid_sortable
+  {
+  public partial class TWebUserControl_template_datagrid_sortable: ki_web_ui.usercontrol_class
+    {
+    public class UserControl_template_datagrid_sortable_Static
+      {
+      public const int TCI_SELECT = 0;
+      public const int TCI_ID = 1;
+      public const int TCI_2 = 2;
+      public const int TCI_3 = 3;
+      public const int TCI_4 = 4;
+      }
+
+    private struct p_type
+      {
+      public bool be_interactive;
+      public bool be_loaded;
+      public bool be_sort_order_ascending;
+      public TClass_biz_0s biz_0s;
+      public uint num_0s;
+      public string sort_order;
+      }
+
+    private p_type p;
+
+    private void InjectPersistentClientSideScript()
+      {
+      // EstablishClientSideFunction(k.client_side_function_enumeral_type.EL);
+      // EstablishClientSideFunction(k.client_side_function_enumeral_type.KGS_TO_LBS);
+      // EstablishClientSideFunction(k.client_side_function_enumeral_type.LBS_TO_KGS);
+      // EstablishClientSideFunction
+      // (
+      // 'RecalculateDependentValues()',
+      // k.EMPTY
+      // + 'El("' + TextBox_gain_or_loss_in_lbs.clientid + '").value ='
+      // +  ' El("' + TextBox_gross_landed_weight_in_pounds.clientid + '").value - El("' + TextBox_gross_invoiced_weight_in_lbs.clientid + '").value;'
+      // + k.NEW_LINE
+      // + 'El("' + TextBox_gain_or_loss_in_kgs.clientid + '").value ='
+      // +  ' El("' + TextBox_gross_landed_weight_in_kgs.clientid + '").value - El("' + TextBox_gross_invoiced_weight_in_kgs.clientid + '").value;'
+      // + k.NEW_LINE
+      // + 'El("' + TextBox_gain_or_loss_per_bale_in_lbs.clientid + '").value ='
+      // +  ' El("' + TextBox_gain_or_loss_in_lbs.clientid + '").value/El("' + TextBox_bales.clientid + '").value;'
+      // + k.NEW_LINE
+      // + 'El("' + TextBox_gain_or_loss_per_bale_in_kgs.clientid + '").value ='
+      // +  ' El("' + TextBox_gain_or_loss_in_kgs.clientid + '").value/El("' + TextBox_bales.clientid + '").value;'
+      // + k.NEW_LINE
+      // + 'El("' + TextBox_actual_gain_or_loss_in_lbs.clientid + '").value ='
+      // +  ' El("' + TextBox_gain_or_loss_in_lbs.clientid + '").value - El("' + TextBox_franchise_in_lbs.clientid + '").value;'
+      // + k.NEW_LINE
+      // + 'El("' + TextBox_actual_gain_or_loss_in_kgs.clientid + '").value ='
+      // +  ' El("' + TextBox_gain_or_loss_in_kgs.clientid + '").value - El("' + TextBox_franchise_in_kgs.clientid + '").value;'
+      // + k.NEW_LINE
+      // + 'El("' + TextBox_actual_gain_or_loss_per_bale_in_lbs.clientid + '").value ='
+      // +  ' El("' + TextBox_actual_gain_or_loss_in_lbs.clientid + '").value/El("' + TextBox_bales.clientid + '").value;'
+      // + k.NEW_LINE
+      // + 'El("' + TextBox_actual_gain_or_loss_per_bale_in_kgs.clientid + '").value ='
+      // +  ' El("' + TextBox_actual_gain_or_loss_in_kgs.clientid + '").value/El("' + TextBox_bales.clientid + '").value;'
+      // + k.NEW_LINE
+      // + 'El("' + TextBox_percent_gain_or_loss.clientid + '").value ='
+      // +  ' Math.round(El("' + TextBox_actual_gain_or_loss_in_lbs.clientid + '").value/El("' + TextBox_net_invoiced_in_lbs.clientid + '").value*100*100)/100;'
+      // + k.NEW_LINE
+      // + 'El("' + TextBox_monetary_gain_or_loss.clientid + '").value ='
+      // +  ' El("' + TextBox_actual_gain_or_loss_in_lbs.clientid + '").value*El("' + TextBox_unit_price_in_cents_per_pound.clientid + '").value;'
+      // );
+      // //
+      // TextBox_bales.attributes.Add('onkeyup','RecalculateDependentValues();');
+      // TextBox_gross_landed_weight_in_pounds.attributes.Add
+      // (
+      // 'onkeyup',
+      // 'El("' + TextBox_gross_landed_weight_in_kgs.clientid + '").value = LbsToKgs(El("' + TextBox_gross_landed_weight_in_pounds.clientid + '").value);'
+      // + ' RecalculateDependentValues();'
+      // );
+      // TextBox_gross_landed_weight_in_kgs.attributes.Add
+      // (
+      // 'onkeyup',
+      // 'El("' + TextBox_gross_landed_weight_in_pounds.clientid + '").value = KgsToLbs(El("' + TextBox_gross_landed_weight_in_kgs.clientid + '").value);'
+      // + ' RecalculateDependentValues();'
+      // );
+      // TextBox_landed_or_ciq_tare.attributes.Add
+      // (
+      // 'onkeyup',
+      // 'El("' + TextBox_landed_or_ciq_tare_in_kgs.clientid + '").value = LbsToKgs(El("' + TextBox_landed_or_ciq_tare.clientid + '").value);'
+      // + ' RecalculateDependentValues();'
+      // );
+      // TextBox_landed_or_ciq_tare_in_kgs.attributes.Add
+      // (
+      // 'onkeyup',
+      // 'El("' + TextBox_landed_or_ciq_tare.clientid + '").value = KgsToLbs(El("' + TextBox_landed_or_ciq_tare_in_kgs.clientid + '").value);'
+      // + ' RecalculateDependentValues();'
+      // );
+      // TextBox_net_landed_in_pounds.attributes.Add
+      // (
+      // 'onkeyup',
+      // 'El("' + TextBox_net_landed_in_kgs.clientid + '").value = LbsToKgs(El("' + TextBox_net_landed_in_pounds.clientid + '").value);'
+      // + ' RecalculateDependentValues();'
+      // );
+      // TextBox_net_landed_in_kgs.attributes.Add
+      // (
+      // 'onkeyup',
+      // 'El("' + TextBox_net_landed_in_pounds.clientid + '").value = KgsToLbs(El("' + TextBox_net_landed_in_kgs.clientid + '").value);'
+      // + ' RecalculateDependentValues();'
+      // );
+      }
+
+    protected void Page_Load(object sender, System.EventArgs e)
+      {
+      if (!p.be_loaded)
+        {
+        if (!p.be_interactive)
+          {
+          DataGrid_control.AllowSorting = false;
+          }
+        Bind();
+        p.be_loaded = true;
+        }
+      InjectPersistentClientSideScript();
+      }
+
+    protected override void OnInit(System.EventArgs e)
+      {
+      // Required for Designer support
+      InitializeComponent();
+      base.OnInit(e);
+      if (Session["UserControl_template_datagrid_sortable.p"] != null)
+        {
+        p = (p_type)(Session["UserControl_template_datagrid_sortable.p"]);
+#warning Revise the ClientID path to this control appropriately.
+        p.be_loaded = IsPostBack && ((Session["UserControl_member_binder_PlaceHolder_content"] as string) == "UserControl_template_datagrid_sortable");
+        }
+      else
+        {
+        p.biz_0s = new TClass_biz_0s();
+        p.be_interactive = !(Session["mode:report"] != null);
+        p.be_loaded = false;
+        p.be_sort_order_ascending = true;
+        p.sort_order = "field_0%";
+        }
+      }
+
+    // / <summary>
+    // / Required method for Designer support -- do not modify
+    // / the contents of this method with the code editor.
+    // / </summary>
+    private void InitializeComponent()
+      {
+      this.DataGrid_control.ItemDataBound += new System.Web.UI.WebControls.DataGridItemEventHandler(this.DataGrid_control_ItemDataBound);
+      this.DataGrid_control.SortCommand += new System.Web.UI.WebControls.DataGridSortCommandEventHandler(this.DataGrid_control_SortCommand);
+      this.DataGrid_control.ItemCommand += new System.Web.UI.WebControls.DataGridCommandEventHandler(this.DataGrid_control_ItemCommand);
+      this.PreRender += this.TWebUserControl_template_datagrid_sortable_PreRender;
+      //this.Load += this.Page_Load;
+      }
+
+    private void TWebUserControl_template_datagrid_sortable_PreRender(object sender, System.EventArgs e)
+      {
+      SessionSet("UserControl_template_datagrid_sortable.p", p);
+      }
+
+    public TWebUserControl_template_datagrid_sortable Fresh()
+      {
+      Session.Remove("UserControl_template_datagrid_sortable.p");
+      return this;
+      }
+
+    private void DataGrid_control_ItemCommand(object source, System.Web.UI.WebControls.DataGridCommandEventArgs e)
+      {
+      if (new ArrayList(new object[] {ListItemType.AlternatingItem, ListItemType.Item, ListItemType.EditItem, ListItemType.SelectedItem}).Contains(e.Item.ItemType))
+        {
+        SessionSet("0_id", k.Safe(e.Item.Cells[UserControl_template_datagrid_sortable_Static.TCI_ID].Text, k.safe_hint_type.NUM));
+        DropCrumbAndTransferTo("template_datagrid_sortable_detail.aspx");
+        }
+      }
+
+    private void DataGrid_control_ItemDataBound(object sender, System.Web.UI.WebControls.DataGridItemEventArgs e)
+      {
+      LinkButton link_button;
+      if (p.be_interactive)
+        {
+        if (new ArrayList(new object[] {ListItemType.AlternatingItem, ListItemType.Item, ListItemType.EditItem, ListItemType.SelectedItem}).Contains(e.Item.ItemType))
+          {
+          link_button = ((e.Item.Cells[UserControl_template_datagrid_sortable_Static.TCI_SELECT].Controls[0]) as LinkButton);
+          link_button.Text = k.ExpandTildePath(link_button.Text);
+          ScriptManager.GetCurrent(Page).RegisterPostBackControl(link_button);
+          p.num_0s++;
+          }
+        }
+      else
+        {
+        e.Item.Cells[UserControl_template_datagrid_sortable_Static.TCI_SELECT].Visible = false;
+        }
+      }
+
+    private void DataGrid_control_SortCommand(object source, System.Web.UI.WebControls.DataGridSortCommandEventArgs e)
+      {
+      if (e.SortExpression == p.sort_order)
+        {
+        p.be_sort_order_ascending = !p.be_sort_order_ascending;
+        }
+      else
+        {
+        p.sort_order = k.Safe(e.SortExpression, k.safe_hint_type.KI_SORT_EXPRESSION);
+        p.be_sort_order_ascending = true;
+        }
+      DataGrid_control.EditItemIndex =  -1;
+      Bind();
+      }
+
+    private void Bind()
+      {
+      p.biz_0s.BindGrid(p.sort_order, p.be_sort_order_ascending, DataGrid_control);
+      Literal_num_0s.Text = p.num_0s.ToString();
+      p.num_0s = 0;
+      }
+
+    } // end TWebUserControl_template_datagrid_sortable
+
+  }
