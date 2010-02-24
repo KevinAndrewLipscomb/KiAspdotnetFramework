b2ad908306e3b868fc6f829b2f3e2823e03de664
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2010-02-24 18:38:46 +0000

    Upgraded MySql.Data.dll.  Added Class_fs based on work done in WebEMSOF.  Added code to deploy.bat to deploy usercontrol/ki/image files.

---
+++ component/fs/Class_fs.cs
@@ -0,0 +1,28 @@
+using System;
+using System.IO;
+
+namespace KiAspdotnetFramework.component.os
+  {
+  public static class Class_os_Static
+    {
+    public const string WWWROOT = "c:\\inetpub\\wwwroot\\";
+    }
+
+  public class Class_fs
+    {
+    internal void PurgeDanglingAttachments(string path)
+      {
+      var flag_file_spec_array = Directory.GetFiles(path,"noninteractive_delete_pending.kaf",SearchOption.AllDirectories);
+      foreach (var flag_file_spec in flag_file_spec_array)
+        {
+        try
+          {
+          Directory.Delete(Path.GetDirectoryName(flag_file_spec),true);
+          }
+        catch (IOException)
+          {
+          }
+        }
+      }
+    }
+  }
