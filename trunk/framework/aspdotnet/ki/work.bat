c9c84f30922f1eaf064b8387659e031b10b0b680
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2010-10-27 22:08:40 +0000

    Restored template_trivial_item components, never converted from Delphi, as C# templates.  Removed obsolete pas-template files.  Updated work.bat to reflect new MySQL path name.

--- trunk/framework/aspdotnet/ki/work.bat
+++ trunk/framework/aspdotnet/ki/work.bat
@@ -5,4 +5,4 @@ cd "C:\Inetpub\wwwroot\KiAspdotnetFramework"
 start /max explorer /e,/select,C:\Inetpub\wwwroot\KiAspdotnetFramework\.svn
 start /max explorer ftp://kalipso:@frompaper2web.com/ssl/KiAspdotnetFramework_x
 start /max KiAspdotnetFramework.sln
-start "" /max "C:\Program Files\MySQL\MySQL Workbench 5.2 OSS\MySQLWorkbench.exe"
+start "" /max "C:\Program Files\MySQL\MySQL Workbench 5.2 CE\MySQLWorkbench.exe"
