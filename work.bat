a22999b49b1a7fe7a9824b7b0597fe0ff8952805
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2011-11-19 00:07:21 +0000

    Revised create-table-template-trivial-item.sql-template to add a UNIQUE clause and an INSERT clause.  Revised work.bat to make a more general reference to the MySQL Workbench that can be resolved via a junction point.

--- work.bat
+++ work.bat
@@ -5,4 +5,4 @@ cd "C:\Inetpub\wwwroot\KiAspdotnetFramework"
 start /max explorer /e,/select,C:\Inetpub\wwwroot\KiAspdotnetFramework\.svn
 start /max explorer ftp://kalipso:@sslws038.alentus.com/ssl/KiAspdotnetFramework_x
 start /max KiAspdotnetFramework.sln
-start "" /max "C:\Program Files\MySQL\MySQL Workbench 5.2 CE\MySQLWorkbench.exe"
+start "" /max "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe"
