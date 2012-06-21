758cddb5141d0c58fb2bb463b9b384f19909584c
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2012-06-21 16:32:28 +0000

    Revised IAW lessons learned from instantiation of KvrsWebsite.

--- work.bat
+++ work.bat
@@ -3,6 +3,5 @@ REM $Id$
 REM
 cd "C:\Inetpub\wwwroot\KiAspdotnetFramework"
 start /max explorer /e,/select,C:\Inetpub\wwwroot\KiAspdotnetFramework\.svn
-start /max explorer ftp://kalipso:@sslws038.alentus.com/ssl/KiAspdotnetFramework_x
 start /max KiAspdotnetFramework.sln
 start "" /max "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe"
