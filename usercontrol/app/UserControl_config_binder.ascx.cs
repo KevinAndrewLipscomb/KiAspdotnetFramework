a011d615ef6c4798fdeeccb7d7ca263679ed2672
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2014-10-27 19:56:35 +0000

    Updated UserControl_config_binder to current coding standard.  In work.bat, made call to MySqlWorkbench more generic.

--- usercontrol/app/UserControl_config_binder.ascx.cs
+++ usercontrol/app/UserControl_config_binder.ascx.cs
@@ -1,124 +1,168 @@
 using kix;
-using System;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.HtmlControls;
-using System.Web.UI.WebControls;
-using System.Collections;
-
 using UserControl_roles_and_matrices_binder;
 using UserControl_users_and_mapping_binder;
+
 namespace UserControl_config_binder
-{
-    public struct p_type
+  {
+
+  public partial class TWebUserControl_config_binder: ki_web_ui.usercontrol_class
     {
-        public bool be_loaded;
-        public string content_id;
-        public uint tab_index;
-    } // end p_type
 
-    public class TWebUserControl_config_binder: ki_web_ui.usercontrol_class
-    {
-        private p_type p;
-        protected System.Web.UI.WebControls.PlaceHolder PlaceHolder_content = null;
-        protected AjaxControlToolkit.TabContainer TabContainer_control = null;
-        protected AjaxControlToolkit.TabPanel TabPanel_users_and_mappings = null;
-        private void Page_Load(object sender, System.EventArgs e)
+    //--
+    //
+    // PRIVATE
+    //
+    //--
+
+    private static class Static
+      {
+      public const int TSSI_ROLES_AND_MATRICES = 0;
+      public const int TSSI_USERS_AND_MAPPING = 1;
+      }
+
+    private struct p_type
+      {
+      public bool be_loaded;
+      public string content_id;
+      public uint tab_index;
+      }
+
+    private p_type p;
+
+    private void FillPlaceHolder
+      (
+      bool be_fresh_control_required,
+      string target
+      )
+      {
+      if (p.tab_index == Static.TSSI_ROLES_AND_MATRICES)
         {
-            if (!p.be_loaded)
-            {
-                if (k.Has((string[])(Session["privilege_array"]), "config-users"))
-                {
-                    TabPanel_users_and_mappings.Enabled = true;
-                }
-                p.be_loaded = true;
-            }
-
+        var c = ((TWebUserControl_roles_and_matrices_binder)(LoadControl("~/usercontrol/app/UserControl_roles_and_matrices_binder.ascx")));
+        p.content_id = AddIdentifiedControlToPlaceHolder(c,"UserControl_roles_and_matrices_binder",PlaceHolder_content,(be_fresh_control_required ? InstanceId() : k.EMPTY));
+        //c.SetTarget(target);
         }
-
-        protected override void OnInit(System.EventArgs e)
+      else if (p.tab_index == Static.TSSI_USERS_AND_MAPPING)
         {
-            // Required for Designer support
-            InitializeComponent();
-            base.OnInit(e);
-            if (Session[InstanceId() + ".p"] != null)
-            {
-                p = (p_type)(Session[InstanceId() + ".p"]);
-                p.be_loaded = IsPostBack && ((Session["UserControl_member_binder_PlaceHolder_content"] as string) == "UserControl_config_binder");
-                switch(p.tab_index)
-                {
-                    case Units.UserControl_config_binder.TSSI_ROLES_AND_MATRICES:
-                        // Dynamic controls must be re-added on each postback.
-                        p.content_id = AddIdentifiedControlToPlaceHolder(((TWebUserControl_roles_and_matrices_binder)(LoadControl("~/usercontrol/app/UserControl_roles_and_matrices_binder.ascx"))), "UserControl_roles_and_matrices_binder", PlaceHolder_content);
-                        break;
-                    case Units.UserControl_config_binder.TSSI_USERS_AND_MAPPING:
-                        p.content_id = AddIdentifiedControlToPlaceHolder(((TWebUserControl_users_and_mapping_binder)(LoadControl("~/usercontrol/app/UserControl_users_and_mapping_binder.ascx"))), "UserControl_users_and_mapping_binder", PlaceHolder_content);
-                        break;
-                }
-            }
-            else
-            {
-                p.be_loaded = false;
-                p.tab_index = Units.UserControl_config_binder.TSSI_ROLES_AND_MATRICES;
-                p.content_id = AddIdentifiedControlToPlaceHolder(((TWebUserControl_roles_and_matrices_binder)(LoadControl("~/usercontrol/app/UserControl_roles_and_matrices_binder.ascx"))),"UserControl_roles_and_matrices_binder",PlaceHolder_content,InstanceId());
-            }
-
+        var c = ((TWebUserControl_users_and_mapping_binder)(LoadControl("~/usercontrol/app/UserControl_users_and_mapping_binder.ascx")));
+        p.content_id = AddIdentifiedControlToPlaceHolder(c,"UserControl_users_and_mapping_binder",PlaceHolder_content,(be_fresh_control_required ? InstanceId() : k.EMPTY));
+        //c.SetTarget(target);
         }
+      }
+    private void FillPlaceHolder(bool be_fresh_control_required)
+      {
+      FillPlaceHolder(be_fresh_control_required,k.EMPTY);
+      }
 
-        // / <summary>
-        // / Required method for Designer support -- do not modify
-        // / the contents of this method with the code editor.
-        // / </summary>
-        private void InitializeComponent()
+    // / <summary>
+    // / Required method for Designer support -- do not modify
+    // / the contents of this method with the code editor.
+    // / </summary>
+    private void InitializeComponent()
+      {
+      TabContainer_control.ActiveTabChanged += TabContainer_control_ActiveTabChanged;
+      PreRender += TWebUserControl_config_binder_PreRender;
+      }
+
+    private void Page_Load(object sender, System.EventArgs e)
+      {
+      if (!p.be_loaded)
         {
-            this.TabContainer_control.ActiveTabChanged += this.TabContainer_control_ActiveTabChanged;
-            this.PreRender += this.TWebUserControl_config_binder_PreRender;
-            //this.Load += this.Page_Load;
+        TabPanel_users_and_mappings.Enabled = k.Has((string[])(Session["privilege_array"]), "config-users");
+        p.be_loaded = true;
         }
+      }
 
-        private void TWebUserControl_config_binder_PreRender(object sender, System.EventArgs e)
+    private void TabContainer_control_ActiveTabChanged(object sender, System.EventArgs e)
+      {
+      p.tab_index = (uint)(TabContainer_control.ActiveTabIndex);
+      PlaceHolder_content.Controls.Clear();
+      FillPlaceHolder(true);
+      }
+
+    private void TWebUserControl_config_binder_PreRender(object sender, System.EventArgs e)
+      {
+      //
+      // Indicate to children which content control was active on this pass, so that on subsequent passes a child can detect whether or
+      // not it is already loaded in the user's browser.
+      //
+      SessionSet(PlaceHolder_content.ClientID, p.content_id);
+      SessionSet(InstanceId() + ".p", p);
+      }
+
+    //--
+    //
+    // PROTECTED
+    //
+    //--
+
+    protected override void OnInit(System.EventArgs e)
+      {
+      // Required for Designer support
+      InitializeComponent();
+      base.OnInit(e);
+      var instance_id = InstanceId();
+      if (Session[instance_id + ".p"] != null)
         {
-            // Indicate to children which content control was active on this pass, so that on subsequent passes a child can detect whether or
-            // not it is already loaded in the user's browser.
-            SessionSet(PlaceHolder_content.ClientID, p.content_id);
-            SessionSet(InstanceId() + ".p", p);
-
+        p = (p_type)(Session[InstanceId() + ".p"]);
+        p.be_loaded = IsPostBack;  // This test is sufficient if this control is being used statically on its page.
+        //
+        // If this control is being used dynamically under one or more parent binder(s), it must ascertain which instance it is, and whether or not that instance's parent binder
+        // had it loaded already.
+        //
+        if (instance_id == "ASP.protected_overview_aspx.UserControl_member_binder_config_binder")
+          {
+          p.be_loaded &= ((Session["UserControl_member_binder_PlaceHolder_content"] as string) == "UserControl_config_binder");
+          }
+//      else if (instance_id == "ASP.~_aspx.UserControl_~_binder_config_binder")
+//        {
+//        p.be_loaded &= ((Session["UserControl_~_binder_PlaceHolder_content"] as string) == "UserControl_config_binder");
+//        }
+        //
+        // Dynamic controls must be re-added on each postback.
+        //
+        FillPlaceHolder(false);
         }
-
-        public TWebUserControl_config_binder Fresh()
+      else
         {
-            TWebUserControl_config_binder result;
-            Session.Remove(InstanceId() + ".p");
-            result = this;
-            return result;
+        p.be_loaded = false;
+        //
+        p.tab_index = Static.TSSI_ROLES_AND_MATRICES;
+        FillPlaceHolder(true);
         }
+      }
 
-        private void TabContainer_control_ActiveTabChanged(object sender, System.EventArgs e)
+    //--
+    //
+    // PUBLIC
+    //
+    //--
+
+    public TWebUserControl_config_binder Fresh()
+      {
+      Session.Remove(InstanceId() + ".p");
+      return this;
+      }
+
+    public void SetTarget(string target)
+      {
+      if (target != k.EMPTY)
         {
-            p.tab_index = (uint)(TabContainer_control.ActiveTabIndex);
-            PlaceHolder_content.Controls.Clear();
-            switch(p.tab_index)
-            {
-                case Units.UserControl_config_binder.TSSI_ROLES_AND_MATRICES:
-                    p.content_id = AddIdentifiedControlToPlaceHolder(((TWebUserControl_roles_and_matrices_binder)(LoadControl("~/usercontrol/app/UserControl_roles_and_matrices_binder.ascx"))),"UserControl_roles_and_matrices_binder",PlaceHolder_content,InstanceId());
-                    break;
-                case Units.UserControl_config_binder.TSSI_USERS_AND_MAPPING:
-                    p.content_id = AddIdentifiedControlToPlaceHolder(((TWebUserControl_users_and_mapping_binder)(LoadControl("~/usercontrol/app/UserControl_users_and_mapping_binder.ascx"))),"UserControl_users_and_mapping_binder",PlaceHolder_content,InstanceId());
-                    break;
-            }
+        if (target.ToLower().Contains("/roles-and-mappings/"))
+          {
+          p.tab_index = Static.TSSI_ROLES_AND_MATRICES;
+          }
+        else if (target.ToLower().Contains("/users-and-mappings/"))
+          {
+          p.tab_index = Static.TSSI_USERS_AND_MAPPING;
+          }
+        //
+        TabContainer_control.ActiveTabIndex = (int)p.tab_index;
+        PlaceHolder_content.Controls.Clear();
+        FillPlaceHolder(false,target);
+        //
         }
+      }
 
     } // end TWebUserControl_config_binder
 
-}
-
-namespace UserControl_config_binder.Units
-{
-    public class UserControl_config_binder
-    {
-        public const int TSSI_ROLES_AND_MATRICES = 0;
-        public const int TSSI_USERS_AND_MAPPING = 1;
-    } // end UserControl_config_binder
-
-}
-
+  }
