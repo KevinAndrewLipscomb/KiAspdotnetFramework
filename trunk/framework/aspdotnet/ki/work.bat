b6c2ab3570067aaa789de2824fcfdea5fe5a41d4
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2014-11-22 22:50:44 +0000

    Revised to work on both Win32 and Win64.

--- trunk/framework/aspdotnet/ki/work.bat
+++ trunk/framework/aspdotnet/ki/work.bat
@@ -4,4 +4,4 @@ REM
 cd "C:\Inetpub\wwwroot\KiAspdotnetFramework"
 start /max explorer /e,/select,C:\Inetpub\wwwroot\KiAspdotnetFramework\.svn
 start /max KiAspdotnetFramework.sln
-start "" /max "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe"
+IF EXIST "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe" (start "" /max "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe") ELSE start "" /max "C:\Program Files (x86)\MySQL\MySQL Workbench\MySQLWorkbench.exe"
