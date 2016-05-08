78e64f2dd24b655308e0d1b766c2517c85448518
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2016-05-08 21:05:33 +0000

    Reformatted Global.asax.cs.

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,69 +1,66 @@
-using System;
-using System.Collections;
-using System.ComponentModel;
-using System.Web;
-using System.Web.SessionState;
-
-
-using System.Security.Principal;
 using Class_biz_user;
+using System;
+using System.Web;
+
 namespace Global
-{
-    public class TGlobal: System.Web.HttpApplication
+  {
+  public class TGlobal: HttpApplication
     {
-        // / <summary>
-        // / Required method for Designer support -- do not modify
-        // / the contents of this method with the code editor.
-        // / </summary>
-        private void InitializeComponent()
-        {
-        }
 
-        //Constructor  Create()
-        public TGlobal() : base()
-        {
-            // Required for Designer support
-            InitializeComponent();
-            // TODO: Add any constructor code after InitializeComponent call
+    // / <summary>
+    // / Required method for Designer support -- do not modify
+    // / the contents of this method with the code editor.
+    // / </summary>
+    private void InitializeComponent()
+      {
+      }
 
-        }
-        protected void Application_Start(object sender, EventArgs e)
-        {
-        }
+    public TGlobal() : base()
+      {
+      InitializeComponent();
+      }
 
-        protected void Session_Start(object sender, EventArgs e)
-        {
-        }
+    protected void Application_Start(object sender, EventArgs e)
+      {
+      }
 
-        protected void Application_BeginRequest(object sender, EventArgs e)
-        {
-        }
+    protected void Session_Start(object sender, EventArgs e)
+      {
+      }
 
-        protected void Application_EndRequest(object sender, EventArgs e)
-        {
-        }
+    protected void Application_BeginRequest(object sender, EventArgs e)
+      {
+      }
 
-        protected void Application_AuthenticateRequest(object sender, EventArgs e)
-        {
-            if (this.Request.IsAuthenticated)
-            {
-                HttpContext.Current.User = new System.Security.Principal.GenericPrincipal(this.User.Identity, new TClass_biz_user().Roles());
-            }
-        }
+    protected void Application_EndRequest(object sender, EventArgs e)
+      {
+      }
 
-        protected void Application_Error(object sender, EventArgs e)
+    protected void Application_AuthenticateRequest(object sender, EventArgs e)
+      {
+      if (Request.IsAuthenticated)
         {
-            this.Server.Transfer("~/exception.aspx");
+        HttpContext.Current.User = new System.Security.Principal.GenericPrincipal
+          (
+          identity:User.Identity,
+          roles:new TClass_biz_user().Roles()
+          );
         }
+      }
 
-        protected void Session_End(object sender, EventArgs e)
-        {
-        }
+    protected void Application_Error(object sender, EventArgs e)
+      {
+      Server.Transfer(path:"~/exception.aspx");
+      }
 
-        protected void Application_End(object sender, EventArgs e)
-        {
-        }
+    protected void Session_End(object sender, EventArgs e)
+      {
+      }
+
+    protected void Application_End(object sender, EventArgs e)
+      {
+      }
 
     } // end TGlobal
 
-}
+  }
