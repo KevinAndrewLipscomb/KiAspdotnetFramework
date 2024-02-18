916520fe93de46e8747b2d4cb1e63822dec32917
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2024-02-18 15:22:45 -0500

    Updated .gitignore per lessons learned.  Converted work.bat to work.ps1.

--- work.bat
+++
@@ -1,7 +0,0 @@
-REM
-REM $Id$
-REM
-cd "C:\Inetpub\wwwroot\KiAspdotnetFramework"
-start /max explorer /e,/select,C:\Inetpub\wwwroot\KiAspdotnetFramework\.svn
-start /max KiAspdotnetFramework.sln
-IF EXIST "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe" (start "" /max "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe") ELSE start "" /max "C:\Program Files (x86)\MySQL\MySQL Workbench\MySQLWorkbench.exe"
