b609f9359a6068e78287057360e4b8f44b195843
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2024-02-29 16:25:46 -0500

    Simplified opening of File Explorer in work.ps1.

--- work.ps1
+++ work.ps1
@@ -1,5 +1,5 @@
 Set-Location "C:\Inetpub\wwwroot\KiAspdotnetFramework"
-Start-Process -WindowStyle Maximized "C:\Inetpub\wwwroot\KiAspdotnetFramework"
+Start-Process -WindowStyle Maximized "."
 Start-Process -WindowStyle Maximized KiAspdotnetFramework.sln
 IF (Test-Path "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe")
   {
