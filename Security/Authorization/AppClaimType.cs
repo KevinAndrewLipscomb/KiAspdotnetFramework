ad81f70fe535982951dd8b0adccff78fdd407539
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2026-05-06 16:17:34 -0400

    Modernized templates and added files (unmodified from their source, so far) that would be needed in new solution instantiations.

---
+++ Security/Authorization/AppClaimType.cs
@@ -0,0 +1,15 @@
+﻿namespace OscarDotNet.Engine.Security.Authorization
+  {
+  static public class AppClaimType
+    {
+
+    public const string AgencyId = "AppAgencyId";
+    public const string DisplayName = "AppDisplayName";
+    public const string ImitatorUsername = "AppImitatorUsername";
+    public const string MemberId = "AppMemberId";
+    public const string Privilege = "AppPrivilege";
+    public const string SmsTarget = "AppSmsTarget";
+    public const string UserId = "AppUserId";
+
+    }
+  }
---
+++ Security/Authorization/PrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+
+namespace OscarDotNet.Extensions
+  {
+  static public class PrincipalExtensions
+    {
+
+    static public ClaimsIdentity ClaimsIdentity(this IPrincipal principal) => (ClaimsIdentity)principal.Identity;
+
+    }
+  }
